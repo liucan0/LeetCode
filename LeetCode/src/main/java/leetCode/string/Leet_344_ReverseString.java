d8183b0758c552c6f3bfe17fd828641487f20f2b
liucan <893202438@qq.com>
2021-02-25 09:37:11 +0800

    LeetCode 344 反转字符串

---
+++ LeetCode/src/main/java/leetCode/string/Leet_344_ReverseString.java
@@ -0,0 +1,47 @@
+package leetCode.string;
+
+import java.util.Arrays;
+
+/**
+ * leetcode 344 翻转字符串
+ * 编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+ *
+ * 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+ *
+ * 你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：["h","e","l","l","o"]
+ * 输出：["o","l","l","e","h"]
+ * 示例 2：
+ *
+ * 输入：["H","a","n","n","a","h"]
+ * 输出：["h","a","n","n","a","H"]
+ */
+public class Leet_344_ReverseString {
+
+    public void reverseString(char[] s) {
+        if (s == null || s.length == 0) {
+            return;
+        }
+        int begin = 0, end = s.length - 1;
+        char temp = s[0];
+        while (begin < end) {
+            temp = s[begin];
+            s[begin] = s[end];
+            s[end] = temp;
+            begin++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        Leet_344_ReverseString instance = new Leet_344_ReverseString();
+        instance.reverseString(s);
+        System.out.println(Arrays.toString(s));
+    }
+}
