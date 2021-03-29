2eb84c80da1ef78a1541be4e308b2ad85f3e01b8
liucan <893202438@qq.com>
2021-03-29 14:20:22 +0800

    LeetCode 263 丑数

---
+++ LeetCode/src/main/java/leetCode/mathematics/Leet_263_IsUgly.java
@@ -0,0 +1,53 @@
+package leetCode.mathematics;
+
+/**
+ * leetcode 263 丑数
+ * 编写一个程序判断给定的数是否为丑数。
+ *
+ * 丑数就是只包含质因数 2, 3, 5 的正整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 6
+ * 输出: true
+ * 解释: 6 = 2 × 3
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: true
+ * 解释: 8 = 2 × 2 × 2
+ * 示例 3:
+ *
+ * 输入: 14
+ * 输出: false
+ * 解释: 14 不是丑数，因为它包含了另外一个质因数 7。
+ * 说明：
+ *
+ * 1 是丑数。
+ * 输入不会超过 32 位有符号整数的范围: [−231,  231 − 1]。
+ */
+public class Leet_263_IsUgly {
+
+    public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Leet_263_IsUgly instance = new Leet_263_IsUgly();
+        System.out.println(instance.isUgly(14));
+    }
+}
