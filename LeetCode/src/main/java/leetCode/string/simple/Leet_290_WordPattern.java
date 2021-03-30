4932f65c1681d06a4d3ba029d63fe54baf0ad462
liucan <893202438@qq.com>
2021-03-30 14:44:25 +0800

    leetcode 290 单词规律

---
+++ LeetCode/src/main/java/leetCode/string/simple/Leet_290_WordPattern.java
@@ -0,0 +1,60 @@
+package leetCode.string.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * leetcode 290 单词规律
+ * 给定一种规律 pattern 和一个字符串 str ，判断 str 是否遵循相同的规律。
+ *
+ * 这里的 遵循 指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应规律。
+ *
+ * 示例1:
+ *
+ * 输入: pattern = "abba", str = "dog cat cat dog"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:pattern = "abba", str = "dog cat cat fish"
+ * 输出: false
+ * 示例 3:
+ *
+ * 输入: pattern = "aaaa", str = "dog cat cat dog"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: pattern = "abba", str = "dog dog dog dog"
+ * 输出: false
+ * 说明:
+ * 你可以假设 pattern 只包含小写字母， str 包含了由单个空格分隔的小写字母。   
+ */
+public class Leet_290_WordPattern {
+
+    public boolean wordPattern(String pattern, String s) {
+        Map<String, Character> str2ch = new HashMap<String, Character>();
+        Map<Character, String> ch2str = new HashMap<Character, String>();
+        int m = s.length();
+        int i = 0;
+        for (int p = 0; p < pattern.length(); ++p) {
+            char ch = pattern.charAt(p);
+            if (i >= m) {
+                return false;
+            }
+            int j = i;
+            while (j < m && s.charAt(j) != ' ') {
+                j++;
+            }
+            String tmp = s.substring(i, j);
+            if (str2ch.containsKey(tmp) && str2ch.get(tmp) != ch) {
+                return false;
+            }
+            if (ch2str.containsKey(ch) && !tmp.equals(ch2str.get(ch))) {
+                return false;
+            }
+            str2ch.put(tmp, ch);
+            ch2str.put(ch, tmp);
+            i = j + 1;
+        }
+        return i >= m;
+    }
+}
