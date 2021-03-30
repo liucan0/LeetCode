d1c5c3ff6eb9d0b6489b7860bcfbf28624be3657
liucan <893202438@qq.com>
2021-03-30 15:22:56 +0800

    leetcode 326 3的幂

---
+++ LeetCode/src/main/java/leetCode/mathematics/simple/Leet_326_IsPowerOfThree.java
@@ -0,0 +1,59 @@
+package leetCode.mathematics.simple;
+
+/**
+ * leetcode 326 3的幂
+ * 给定一个整数，写一个函数来判断它是否是 3 的幂次方。如果是，返回 true ；否则，返回 false 。
+ *
+ * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：n = 27
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：n = 0
+ * 输出：false
+ * 示例 3：
+ *
+ * 输入：n = 9
+ * 输出：true
+ * 示例 4：
+ *
+ * 输入：n = 45
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * -231 <= n <= 231 - 1
+ *  
+ *
+ * 进阶：
+ *
+ * 你能不使用循环或者递归来完成本题吗？
+ */
+public class Leet_326_IsPowerOfThree {
+
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
+            return false;
+        }
+        int temp = n;
+        while (temp > 1) {
+            int pop = temp % 3;
+            if (pop != 0) {
+                return false;
+            }
+            temp /= 3;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Leet_326_IsPowerOfThree instance = new Leet_326_IsPowerOfThree();
+        System.out.println(instance.isPowerOfThree(1));
+    }
+}
